315c8bd340d11af9125e2deb25ea16285ff84fdc
jakareya76 <jakareya1306@gmail.com>
2024-04-05 15:48:09 +0600

    design left side nav

--- src/components/BreakingNews.jsx
+++ src/components/BreakingNews.jsx
@@ -4,12 +4,12 @@ const BreakingNews = () => {
   return (
     <div className="flex items-center bg-[#F3F3F3] p-5 rounded my-8 gap-10">
       <button className="px-10 text-lg text-white btn btn-error">Latest</button>
-      <p className="text-xl font-medium">
-        <Marquee pauseOnHover={true}>
+      <Marquee pauseOnHover={true}>
+        <p className="text-xl font-medium">
           Match Highlights: Germany vs Spain — as it happened ! Match
           Highlights: Germany vs Spain as
-        </Marquee>
-      </p>
+        </p>
+      </Marquee>
     </div>
   );
 };
--- src/components/LeftSideNav.jsx
+++ src/components/LeftSideNav.jsx
@@ -1,5 +1,36 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
 const LeftSideNav = () => {
-  return <div>LeftSideNav</div>;
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    const getCategories = async () => {
+      const res = await fetch("/data/categories.json");
+      const data = await res.json();
+
+      setCategories(data);
+    };
+
+    getCategories();
+  }, []);
+
+  console.log(categories);
+
+  return (
+    <div>
+      <h2 className="text-2xl font-bold">All Categories</h2>
+      {categories.map((category) => {
+        return (
+          <Link to="/" key={category.id}>
+            <div className="p-4 my-4 text-xl font-semibold bg-gray-100 border rounded-lg cursor-pointer">
+              <h2>{category.name}</h2>
+            </div>
+          </Link>
+        );
+      })}
+    </div>
+  );
 };
 
 export default LeftSideNav;
